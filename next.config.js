06ccd4ba7496e75c16043efa9b566dcbd87bec91
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-08-01 09:41:48 +0200

    :hammer: chore: bump deps

--- next.config.js
+++ next.config.js
@@ -3,17 +3,4 @@ module.exports = {
   images: {
     domains: ["flagcdn.com"],
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      };
-    }
-
-    return config;
-  },
 };
